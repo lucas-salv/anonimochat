ec02e985f971f21e1c09f17d3f6278c7168ad75a
Salv-Dev <salv.dev.br@gmail.com>
2021-01-26 20:26:05 -0300

    create Login component

---
+++ screens/Login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export default function Login() {
+    return (
+        <Container>
+            <Title>AnonimoChat</Title>
+        </Container>
+    )
+}
