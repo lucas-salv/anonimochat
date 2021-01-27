100c93f9d6a0188fb7b5dfe84c0004be09dd6d4a
Salv-Dev <salv.dev.br@gmail.com>
2021-01-26 21:56:42 -0300

    NicknameForm component created

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Container, ContainerForm, Input, Button } from './styles';
-import { Entypo } from '@expo/vector-icons';
+import { Container } from './styles';
 
 import Title from './../../components/Title';
 import ContainerSVG from './../../components/ContainerSVG';
+import NicknameForm from './../../components/NicknameForm';
 
 export default function Login() {
     return (
         <Container>
             <Title />
             <ContainerSVG/>
-            <ContainerForm>
-                <Input placeholder="Digite um apelido..." />
-                <Button>
-                    <Entypo name="controller-play" size={20} color="#FF3366" />
-                </Button>
-            </ContainerForm>
+            <NicknameForm />
         </Container>
     )
 }
--- screens/Login/styles.js
+++ screens/Login/styles.js
@@ -8,25 +8,3 @@ export const Container = styled.View`
     padding: 60px;
     font-family: Lato sans-serif;
 `;
-
-export const ContainerForm = styled.View`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    background-color: ${props => props.theme.secondaryColor};
-    padding: 10px;
-    border-radius: 5px;
-`;
-
-export const Input = styled.TextInput.attrs(props => ({
-    placeholderTextColor: props.theme.fontColor
-}))`
-    width: 90%;
-    color: ${props => props.theme.fontColor};
-`;
-
-export const Button = styled.TouchableOpacity`
-    display: flex;
-    align-items: flex-end;
-    width: 10%;
-`;
