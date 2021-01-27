2c2e94bfc83454ce19cee6df8bb276080c65884f
Salv-Dev <salv.dev.br@gmail.com>
2021-01-26 21:47:26 -0300

    Title component created

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Container, ContainerTitle, ContainerSVG, ContainerForm, Title, Desc, Span, Input, Button } from './styles';
+import { Container, ContainerSVG, ContainerForm, Input, Button } from './styles';
 import { Entypo } from '@expo/vector-icons';
+
+import Title from './../../components/Title';
 
 export default function Login() {
     return (
         <Container>
-            <ContainerTitle>
-                <Title>Anônimo<Span>Chat</Span></Title>
-                <Desc>
-                    Seja bem-vindo(a) ao bate-papo em
-                    tempo real. Por favor, para
-                    continuar preencha o campo de apelido abaixo:
-                </Desc>
-            </ContainerTitle>
+            <Title />
             <ContainerSVG></ContainerSVG>
             <ContainerForm>
                 <Input placeholder="Digite um apelido..." />
--- screens/Login/styles.js
+++ screens/Login/styles.js
@@ -9,10 +9,6 @@ export const Container = styled.View`
     font-family: Lato sans-serif;
 `;
 
-export const ContainerTitle = styled.View`
-
-`;
-
 export const ContainerSVG = styled.View`
     width: 100px;
     height: 100px;
@@ -25,24 +21,6 @@ export const ContainerForm = styled.View`
     background-color: ${props => props.theme.secondaryColor};
     padding: 10px;
     border-radius: 5px;
-`;
-
-export const Title = styled.Text`
-    text-align: center;
-    color: ${props => props.theme.mainColor};
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 5px;
-`;
-
-export const Span = styled.Text`
-    color: ${props => props.theme.mainColor}99;
-    font-style: italic;
-`;
-
-export const Desc = styled.Text`
-    text-align: center;
-    color: ${props  => props.theme.fontColor};
 `;
 
 export const Input = styled.TextInput.attrs(props => ({
