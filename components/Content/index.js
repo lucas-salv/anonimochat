f8f59185acafca5c90ccbfb9c379f6ad7f380f8e
Salv-Dev <salv.dev.br@gmail.com>
2021-01-27 21:53:23 -0300

    content and message component created

---
+++ components/Content/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { Container } from './styles';
+
+import Message from './../Message';
+
+const defaultMessage = [
+    {type: 'you', message: 'Mussum Ipsum, cacilds vidis'},
+    {type: 'anonymous', message: 'Mussum Ipsum, cacilds vidis'},
+    {type: 'you', message: 'Mussum Ipsum, cacilds vidis litro abertis.'},
+    {type: 'anonymous', message: 'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad litora'},
+    {type: 'you', message: 'Mussum Ipsum, cacilds vidis'},
+    {type: 'anonymous', message: 'Mussum Ipsum, cacilds vidis'},
+    {type: 'anonymous', message: 'Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent sociosqu ad litora'},
+]
+
+export default function Context() {
+    return (
+        <Container>
+
+            <FlatList 
+                data={defaultMessage}
+                style={{ padding: 40}}
+                renderItem={({item, index}) => <Message key={index} isYou={item.type === "you" ? true : false} data={item.message} />}
+            />
+    
+        </Container>
+    )
+}
--- screens/Chat/index.js
+++ screens/Chat/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Container } from './styles';
 
 import Header from './../../components/Header';
+import Content from './../../components/Content';
 
 export default function Chat() {
     return (
         <Container>
             <Header />
+            <Content />
         </Container>
     )
 }
