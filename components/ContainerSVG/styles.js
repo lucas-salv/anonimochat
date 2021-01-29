7ffd25d2f96f861227f9fc33ceb920a821d19af0
Salv-Dev <salv.dev.br@gmail.com>
2021-01-28 22:10:15 -0300

    test and old structure removed

--- components/ContainerSVG/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-    width: 100px;
-    height: 100px;
-`;
--- components/ThemeButton/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.TouchableOpacity`
-    justify-content: center;
-    width: 10%;
-`;
