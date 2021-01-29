eecf9fa5555de1ae48fa966805af9e1d6ba0b058
Salv-Dev <salv.dev.br@gmail.com>
2021-01-28 22:46:32 -0300

    stack-navigation configured

--- chat/screens/Login/index.js
+++ chat/screens/Login/index.js
@@ -5,12 +5,12 @@ import Title from './../../components/Title';
 import ContainerSVG from './../../components/ContainerSVG';
 import NicknameForm from './../../components/NicknameForm';
 
-export default function Login() {
+export default function Login({ navigation }) {
     return (
         <Container>
             <Title />
             <ContainerSVG/>
-            <NicknameForm />
+            <NicknameForm nav={navigation}/>
         </Container>
     )
 }
