dd8330bb1191b5c549f375175d9f1c29e35dbfd4
Salv-Dev <salv.dev.br@gmail.com>
2021-02-04 00:40:42 -0300

    changes to avatar images

---
+++ chat/utils/images.js
@@ -0,0 +1,6 @@
+export default images = {
+    "1": require("./../assets/avatar01.png"),
+    "2": require("./../assets/avatar02.png"),
+    "3": require("./../assets/avatar03.png"),
+    "4": require("./../assets/avatar04.png")
+}
--- chat/utils/io.js
+++ chat/utils/io.js
@@ -2,6 +2,4 @@ import ioClient from 'socket.io-client';
 
 const URL = 'https://servidorlocaltunnelaqui';
 
-export const url = URL;
-
 export default ioClient(URL);
